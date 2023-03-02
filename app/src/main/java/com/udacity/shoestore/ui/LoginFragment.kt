b0e7397d1266141d2c48be35d9a48830c82069ac
Paolo Caldera <paolo.caldera@outlook.com>
2023-03-02 19:49:43 +0100

    Create user arguments and comment code

--- app/src/main/java/com/udacity/shoestore/ui/LoginFragment.kt
+++ app/src/main/java/com/udacity/shoestore/ui/LoginFragment.kt
@@ -60,12 +60,14 @@ class LoginFragment : Fragment() {
         binding.passwordInputLayout.isErrorEnabled = false
 
         if (binding.emailEditText.text.isNullOrEmpty()) {
+            // Highlight in red the email box and display the error message
             binding.emailInputLayout.isErrorEnabled = true
             binding.emailInputLayout.error = resources.getString(R.string.error_message)
             return false
         }
 
         if (binding.passwordEditText.text.isNullOrEmpty()) {
+            // Highlight in red the password box and display the error message
             binding.passwordInputLayout.isErrorEnabled = true
             binding.passwordInputLayout.error = resources.getString(R.string.error_message)
             return false
--- app/src/main/java/com/udacity/shoestore/ui/WelcomeFragment.kt
+++ app/src/main/java/com/udacity/shoestore/ui/WelcomeFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.udacity.shoestore.R
 import com.udacity.shoestore.databinding.FragmentWelcomeBinding
 
@@ -12,6 +13,12 @@ class WelcomeFragment : Fragment() {
 
     private lateinit var binding: FragmentWelcomeBinding
     private lateinit var args: WelcomeFragmentArgs
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // Get navigation arguments from bundle
+        args = WelcomeFragmentArgs.fromBundle(requireArguments())
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -22,10 +29,18 @@ class WelcomeFragment : Fragment() {
         binding.welcomeFragment = this@WelcomeFragment
         binding.lifecycleOwner = viewLifecycleOwner
 
-
-
-        binding.welcomeTextView.text = resources.getString(R.string.welcome_text, )
+        // Insert user email in welcome sentence
+        binding.welcomeTextView.text = resources.getString(R.string.welcome_text, args.userEmail)
 
         return binding.root
+    }
+
+
+    /**
+     * READ INSTRUCTIONS button clicked
+     */
+    fun readInstructions() {
+        val action = WelcomeFragmentDirections.actionWelcomeFragmentToInstructionsFragment()
+        findNavController().navigate(action)
     }
 }
