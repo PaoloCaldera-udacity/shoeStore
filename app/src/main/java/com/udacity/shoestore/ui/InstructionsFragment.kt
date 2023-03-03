4fc12ace41083addc37fe941d6311bbd1584de49
Paolo Caldera <paolo.caldera@outlook.com>
2023-03-03 11:59:22 +0100

    Add fragment_instructions.xml layout and create action

--- app/src/main/java/com/udacity/shoestore/ui/InstructionsFragment.kt
+++ app/src/main/java/com/udacity/shoestore/ui/InstructionsFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.udacity.shoestore.databinding.FragmentInstructionsBinding
 
 class InstructionsFragment : Fragment() {
@@ -17,6 +18,14 @@ class InstructionsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentInstructionsBinding.inflate(inflater, container, false)
+        binding.instructionsFragment = this@InstructionsFragment
+        binding.lifecycleOwner = viewLifecycleOwner
+
         return binding.root
+    }
+
+    fun start() {
+        val action = InstructionsFragmentDirections.actionInstructionsFragmentToShoeListFragment()
+        findNavController().navigate(action)
     }
 }
