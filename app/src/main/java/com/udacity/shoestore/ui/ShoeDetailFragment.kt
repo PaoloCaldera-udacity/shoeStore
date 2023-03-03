4a0c217a7a301d5b45198cb234b7a47dc5d1914c
Paolo Caldera <paolo.caldera@outlook.com>
2023-03-03 18:46:46 +0100

    Add fragment_shoe_detail.xml layout and main logic

--- app/src/main/java/com/udacity/shoestore/ui/ShoeDetailFragment.kt
+++ app/src/main/java/com/udacity/shoestore/ui/ShoeDetailFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.udacity.shoestore.databinding.FragmentShoeDetailBinding
 
 class ShoeDetailFragment : Fragment() {
@@ -17,6 +18,21 @@ class ShoeDetailFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentShoeDetailBinding.inflate(inflater, container, false)
+        binding.shoeDetailFragment = this@ShoeDetailFragment
+        binding.lifecycleOwner = viewLifecycleOwner
+
         return binding.root
+    }
+
+    fun cancel() {
+        val action = ShoeDetailFragmentDirections.actionShoeDetailFragmentToShoeListFragment()
+        findNavController().navigate(action)
+    }
+
+    fun save() {
+        // Check if the fields have been compiled correctly
+        // Call the viewModel method which adds a new shoe
+        val action = ShoeDetailFragmentDirections.actionShoeDetailFragmentToShoeListFragment()
+        findNavController().navigate(action)
     }
 }
