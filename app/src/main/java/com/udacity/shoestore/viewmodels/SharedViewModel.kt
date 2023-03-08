1d63aee00e6bf5da8493836e63c40c9c93497efc
Paolo Caldera <paolo.caldera@outlook.com>
2023-03-08 12:11:40 +0100

    Possible solution for 2-way data binding

--- app/src/main/java/com/udacity/shoestore/viewmodels/SharedViewModel.kt
+++ app/src/main/java/com/udacity/shoestore/viewmodels/SharedViewModel.kt
@@ -9,13 +9,15 @@ import com.udacity.shoestore.models.Shoe
 
 class SharedViewModel : ViewModel() {
 
-    // LiveData variable, containing the list of Shoe objects
+    // LiveData variable: list of Shoe objects
     private val _shoeList: MutableLiveData<MutableList<Shoe>> = MutableLiveData(mutableListOf())
     val shoeList: LiveData<MutableList<Shoe>> get() = _shoeList
 
-    // LiveData variable, containing the entries of the new Shoe object
-    private val _newShoeEntry: MutableLiveData<Shoe> = MutableLiveData()
-    val newShoeEntry: LiveData<Shoe> get() = _newShoeEntry
+    // LiveData variable: new Shoe entry (2-way data binding)
+    val shoeNameEntry: MutableLiveData<String> = MutableLiveData()
+    val shoeCompanyEntry: MutableLiveData<String> = MutableLiveData()
+    val shoeSizeEntry: MutableLiveData<String> = MutableLiveData()
+    val shoeDescriptionEntry: MutableLiveData<String> = MutableLiveData()
 
     // Login status. true = logged in; false = logged out
     private var loginStatus: Boolean = false
@@ -25,7 +27,14 @@ class SharedViewModel : ViewModel() {
      * Adds a Shoe item to the shoes list
      */
     fun addToList() {
-        _shoeList.value?.add(newShoeEntry.value!!)
+        _shoeList.value?.add(Shoe(
+            name = shoeNameEntry.value!!,
+            size = shoeSizeEntry.value!!.toDouble(),
+            company = shoeCompanyEntry.value!!,
+            description = shoeDescriptionEntry.value!!
+        ))
+
+        cleanEntry()
     }
 
 
@@ -34,6 +43,14 @@ class SharedViewModel : ViewModel() {
      */
     fun editLoginStatus() {
         loginStatus = !loginStatus
+    }
+
+
+    private fun cleanEntry() {
+        shoeNameEntry.value = ""
+        shoeSizeEntry.value = ""
+        shoeCompanyEntry.value = ""
+        shoeDescriptionEntry.value = ""
     }
 
 }
